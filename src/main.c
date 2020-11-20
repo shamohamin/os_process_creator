92a3b0cf3218ac3eba2e8e00654fd483bb10e198
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-11-20 13:24:26 +0330

    channged src directory

---
+++ src/main.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include "main.h"
+
+int main()
+{
+    Command *ptr;
+    ptr = (Command *)malloc(sizeof(Command));
+
+    char *file_path = "command.txt";
+    reading_file(file_path, ptr);
+
+    creating_process(ptr);
+}
+
+void creating_process(Command *command_st)
+{
+    int buff_size = LINE_SIZE * command_st->child_proccess_count;
+    char write_msg[buff_size];
+    char write_msg_child[buff_size];
+    char read_msg_child[buff_size];
+    int pipe_buffer_size = 0;
+    char read_msg[buff_size];
+    char omadn[LINE_SIZE];
+    childs = (ChildInfo **)malloc(
+        sizeof(ChildInfo *) * command_st->commands_count);
+    for (int i = 0; i < command_st->commands_count; i++)
+        childs[i] = NULL;
+    clock_t start = clock();
+    pid_t main_routing_id = getpid();
+
+    int fd[2];
+
+    if (pipe(fd) == -1)
+        ERROR_HANDLER_AND_DIE("couldn't make pipe;");
+
+    for (int i = 0; i < command_st->proccess_count; i++)
+    {
+        memset(write_msg, 0, buff_size);
+
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            ERROR_HANDLER_AND_DIE("cant make the process.");
+        }
+        else if (pid == 0) // child
+        {
+            int fd_child[2];
+            pid_t id_of_process[command_st->child_proccess_count];
+
+            if (pipe(fd_child) == -1)
+                ERROR_HANDLER_AND_DIE("couldn't make pipe;");
+
+            read(fd[READ_END], read_msg, sizeof(write_msg));
+
+            for (int j = 0; j < command_st->child_proccess_count; j++)
+            {
+                memset(write_msg_child, 0, buff_size);
+
+                pid_t child_pid = fork();
+
+                if (child_pid < 0)
+                {
+                    ERROR_HANDLER_AND_DIE("cant make the childs process.");
+                }
+                else if (child_pid == 0) // child of child
+                {
+                    child_process(read_msg_child, fd_child, buff_size);
+                }
+                strcpy(
+                    write_msg_child,
+                    split_the_generated_commnad(read_msg, j, command_st->child_proccess_count));
+
+                write(fd_child[WRITE_END], write_msg_child, buff_size);
+            }
+
+            for (int j = 0; j < command_st->child_proccess_count; j++)
+            {
+                wait(NULL);
+            }
+
+            exit(0);
+        }
+
+        command_st->commands_count = enque(
+            create_new_process_info(pid, main_routing_id, IS_NOT_CHILD, IS_PARRENT, i + 1, -1),
+            command_st->commands_count);
+
+        int start_index = i * command_st->child_proccess_count;
+        int end_index = start_index + command_st->child_proccess_count;
+        strcpy(write_msg, generating_commands(start_index, end_index, command_st));
+        write(fd[WRITE_END], write_msg, buff_size);
+    }
+
+    for (int i = 0; i < command_st->proccess_count; i++)
+    {
+        int status;
+        pid_t child_id = wait(&status);
+        set_end_time_and_status_for_terminated_process(child_id, status, command_st->commands_count);
+        // read(fd[READ_END], omadn, strlen(write_  msg));
+        // printf("\n%s\n", omadn);
+    }
+
+    for (int i = 0; childs[i] != NULL; i++)
+        printing_process_info(childs[i]);
+
+    SHOWING_CONSUMING_TIME((double)(clock() - start) / CLOCKS_PER_SEC);
+}
+
+void child_process(char read_msg_child[], int fd_child[], int buff_size)
+{
+    memset(read_msg_child, 0, buff_size);
+    read(fd_child[READ_END], read_msg_child, buff_size);
+    char **args = malloc(sizeof(char *) * MAX_ARGS_LEN);
+    int args_len = parse_command_to_be_executed(read_msg_child, args);
+    int time_of_command = (int)strtol(args[args_len], NULL, 10);
+    args[args_len] = NULL;
+
+    if ((execv(args[0], &args[1]) < 0))
+    {
+        printf("\n warning!!!!!!!: couldnt execute!. command: %s\n", read_msg_child);
+        exit(0);
+    }
+}
+
+ChildInfo *create_new_process_info(
+    pid_t process_id,
+    pid_t parrent_id,
+    int is_child,
+    int is_parrent,
+    int process_number,
+    int parrent_number)
+{
+    ChildInfo *c_process = (ChildInfo *)malloc(sizeof(ChildInfo));
+    if (c_process == NULL)
+        ERROR_HANDLER_AND_DIE("Allocating wasn't successfull;");
+
+    c_process->id = process_id;
+    c_process->parrent_id = parrent_id;
+    c_process->is_parrent = is_parrent;
+    c_process->start_time = clock();
+    c_process->is_child = is_child;
+    c_process->process_number = process_number;
+    c_process->parrent_number = parrent_number;
+
+    return c_process;
+}
+
+// mimic the queue for queue of childs
+int enque(ChildInfo *c_process, int MAX_QUEUE_SIZE)
+{
+    int count = 0;
+
+    for (int i = 0; i < MAX_QUEUE_SIZE; i++, count++)
+    {
+        if (childs[i] == NULL)
+            break;
+    }
+
+    // reallocating
+    if (count > MAX_QUEUE_SIZE)
+    {
+        childs = (ChildInfo **)realloc(childs, (sizeof(ChildInfo *) * count));
+        for (int i = MAX_QUEUE_SIZE; i < count; i++)
+            childs[i] = NULL;
+    }
+
+    if (!childs)
+        ERROR_HANDLER_AND_DIE("reallocating wasn't sucessfull!!!!");
+
+    childs[count] = c_process;
+
+    return count > MAX_QUEUE_SIZE ? count : MAX_QUEUE_SIZE;
+}
+
+void set_end_time_and_status_for_terminated_process(pid_t pid, int exit_status, int MAX_QUEUE_SIZE)
+{
+    int found = 0;
+    for (int i = 0; i < MAX_QUEUE_SIZE; i++)
+    {
+        if (childs[i]->id == pid)
+        {
+            found = 1;
+            childs[i]->end_time = clock();
+            childs[i]->execution_time = (((double)(childs[i]->end_time - childs[i]->start_time)) / CLOCKS_PER_SEC);
+            childs[i]->exit_status = exit_status;
+            break;
+        }
+    }
+
+    if (!found)
+        ERROR_HANDLER_AND_DIE("Couldn't find the process.");
+}
---
+++ src/utility_functions.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+int parse_command_to_be_executed(char *line, char **args)
+{
+    int args_counting = 0;
+    int start_index = 0;
+    int counter = 0;
+    int c = 0;
+
+    for (counter = 0;; counter++)
+    {
+        if (isblank(line[counter]) || line[counter] == '\0')
+        {
+            c = 0;
+            args[args_counting] = (char *)malloc(sizeof(char) * strlen(line));
+
+            for (int i = start_index; i < counter; i++, c++)
+                args[args_counting][c] = line[i];
+
+            if (line[counter] == '\0')
+            {
+                break;
+            }
+            else
+            {
+                args[args_counting][c] = '\0';
+            }
+            start_index = counter + 1;
+            args_counting++;
+        }
+    }
+    args[args_counting][c] = '\0';
+
+    for (int i = 0; i <= args_counting; i++)
+    {
+        if (args[i] == NULL || args[i][0] == '\0' || isblank(args[i][0]))
+        {
+            args_counting--;
+            break;
+        }
+    }
+
+    return args_counting;
+}
+
+char *generating_commands(int start_index, int last_index, Command *command_st)
+{
+
+    char *commands = (char *)malloc(LINE_SIZE * (last_index - start_index) * sizeof(char));
+    int counter = 0;
+
+    for (int i = start_index; i < last_index; i++)
+    {
+        int counter_commnad = 0;
+        while (1)
+        {
+            if (
+                command_st->commands[i][counter_commnad] == '\0' ||
+                command_st->commands[i][counter_commnad] == 0)
+                break;
+
+            commands[counter] = command_st->commands[i][counter_commnad];
+            counter++, counter_commnad++;
+        }
+
+        if (i != last_index - 1)
+        {
+            commands[counter - 1] = ' ';
+            commands[counter] = ';';
+        }
+
+        counter++;
+    }
+
+    commands[counter] = '\0';
+
+    return commands;
+}
+
+char *split_the_generated_commnad(char src[], int pos, int count)
+{
+    char **temp = (char **)malloc(sizeof(char *) * count);
+    for (int i = 0; i < count; i++)
+        temp[i] = (char *)malloc(sizeof(char) * LINE_SIZE * count);
+
+    int counter = 0;
+    int last_index, first_index;
+    int tmp_index = 0;
+    int c = 0;
+
+    for (first_index = 0, last_index = 0;; counter++)
+    {
+        if (src[counter] == '\0')
+            break;
+        if (src[counter] == ';')
+        {
+            last_index = counter;
+            c = 0;
+            for (int i = first_index; i < last_index; i++, c++)
+                temp[tmp_index][c] = src[i];
+
+            temp[tmp_index][c] = '\0';
+            first_index = last_index + 1;
+            tmp_index++;
+        }
+    }
+
+    last_index = counter;
+    c = 0;
+    for (int i = first_index; i < last_index; i++, c++)
+        temp[tmp_index][c] = src[i];
+    temp[tmp_index][c] = '\0';
+
+    for (int i = 0; i < tmp_index && i != pos; i++)
+        free(temp[i]);
+
+    return temp[pos];
+}
+
+void create_childs_process()
+{
+}
+
+/*
+    this function is used for removing the spaces between the args and commands;
+*/
+char *line_convertion(char *line)
+{
+    char *temp = (char *)malloc(sizeof(char) * 100);
+    int i = 0;
+    int counter = 0;
+    int flag = 1;
+
+    while ((line[i] != '\0' || line[i] != 0))
+    {
+        if (!isblank(line[i]))
+        {
+            temp[counter] = line[i];
+            counter++;
+            flag = 1;
+        }
+
+        if (isblank(line[i]) && flag)
+        {
+            temp[counter] = ' ';
+            counter++;
+            flag = 0;
+        }
+
+        i++;
+    }
+    temp[counter++] = '\0';
+
+    return temp;
+}
+/*
+    this function is used for seperating the arguments of command and time for that specific command
+        seperate tha line with using the args;
+*/
+char *seperating_values(char *line, int pos)
+{
+    char *value_holder[SIZE];
+    int counter = 0;
+    int str_counter = 0;
+    int flag = 1;
+
+    for (int i = 0; line[i] != '\0' && line[i] != 0; i++)
+    {
+        if (flag)
+        {
+            value_holder[counter] = (char *)malloc(sizeof(char) * LINE_SIZE);
+            flag = 0;
+        }
+
+        if (isblank(line[i]))
+        {
+            value_holder[counter][str_counter] = '\0';
+            counter++;
+            str_counter = 0;
+            flag = 1;
+            continue;
+        }
+
+        value_holder[counter][str_counter] = line[i];
+        str_counter++;
+    }
+
+    value_holder[counter][str_counter] = '\0';
+
+    char *val = value_holder[pos];
+
+    for (int i = 0; i <= counter && i != pos; i++)
+        free(value_holder[i]);
+
+    return val;
+}
+
+void reading_file(char *filepath, Command *command_st)
+{
+    char *buffer = (char *)malloc(sizeof(char) * LINE_SIZE);
+    size_t input_size = LINE_SIZE;
+    int counter = 0;
+
+    if (buffer == NULL)
+        ERROR_HANDLER_AND_DIE("Allocating wasn't successfull!!");
+
+    FILE *f;
+    if ((f = fopen(filepath, "r")) == NULL)
+        ERROR_HANDLER_AND_DIE("Couldnt open the file.");
+
+    int i = 0;
+    while (fgets(buffer, LINE_SIZE, f))
+    {
+        char *line = buffer;
+        line = line_convertion(line);
+
+        if (i == 0)
+        {
+            command_st->proccess_count = (int)strtol(seperating_values(line, 0), NULL, 10);
+            command_st->child_proccess_count = (int)strtol(seperating_values(line, 1), NULL, 10);
+        }
+        else
+        {
+            command_st->commands[i - 1] = (char *)malloc(strlen(line) * sizeof(char));
+            strcpy(command_st->commands[i - 1], line);
+        }
+
+        // freeing readed line from file
+        free(line);
+        i++;
+    }
+
+    command_st->commands_count = i;
+
+    // freeing the pointers
+    free(buffer);
+    // closing  the file
+    fclose(f);
+}
+
+void printing_process_info(ChildInfo *c_process)
+{
+    printf("***********************\n");
+    printf("id: %d\n", c_process->id);
+    printf("number of process: %d\n", c_process->process_number);
+    printf("is_parrent: %d\n", c_process->is_parrent);
+    printf("start_time: %lu\n", c_process->start_time);
+    printf("end time: %lu\n", c_process->end_time);
+    printf("elapsed time: %f\n", c_process->execution_time);
+    printf("parrent process id: %d\n", c_process->parrent_id);
+    printf("exit status: %d\n", c_process->exit_status);
+    printf("***********************\n");
+}
